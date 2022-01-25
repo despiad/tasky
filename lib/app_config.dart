9936870510edc5d0ba2b2208b8f805b26dd9a00b
Alexander <yatsyuk.a.a.1@gmail.com>
2022-01-25 13:45:42 +0200

    add flavors, change domain name

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -2,13 +2,17 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
-   bool get isProduction =>
+  bool get isProduction =>
       kReleaseMode || environment.toLowerCase().startsWith('prod');
 
-   String get environment => dotenv.env['ENVIRONMENT'] ?? 'dev';
+  String get environment => dotenv.env['ENVIRONMENT'] ?? 'dev';
 
   Future<void> load() async {
-    await dotenv.load(fileName: 'assets/.env');
+    if (kReleaseMode) {
+      await dotenv.load(fileName: 'assets/config/.env.prod');
+    } else {
+      await dotenv.load(fileName: 'assets/config/.env.dev');
+    }
     debugPrint('ENVIRONMENT: $environment');
   }
 }
